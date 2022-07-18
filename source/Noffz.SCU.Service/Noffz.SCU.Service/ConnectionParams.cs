9c0aee97b6af3fdb357018650913be4e8d20788c
Frederic Florack <florackfrederic@gmail.com>
2022-07-18 16:00:19 +0200

    Added connection functionality

---
+++ source/Noffz.SCU.Service/Noffz.SCU.Service/ConnectionParams.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Noffz.SCU.Service
+{
+    public abstract class ConnectionParams
+    {
+        public class COMPort : ConnectionParams
+        {
+            public Int32 comPortNumber;
+
+            public COMPort(int comPortNumber)
+            {
+                this.comPortNumber = comPortNumber;
+            }
+        }
+        public class IP : ConnectionParams
+        {
+            public string IPAddress;
+
+            public IP(string iPAddress)
+            {
+                IPAddress = iPAddress;
+            }
+        }
+    }
+}
--- source/Noffz.SCU.Service/Noffz.SCU.Service/ScuService.cs
+++ source/Noffz.SCU.Service/Noffz.SCU.Service/ScuService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Noffz.SCU.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,27 @@ namespace Noffz.SCU.Service
 {
     public class ScuService
     {
+        private Config config;
+        private ScuSession scu = null;
+
+        public ScuService(ConnectionParams c_params, Config config)
+        {
+            this.config = config;
+            ScuSession.EnableLogging = true;
+            connect(c_params);
+        }
+
+        private void connect(ConnectionParams c_params)
+        {
+            switch (c_params)
+            {
+                case ConnectionParams.COMPort comport:
+                    scu = new ScuSession(comport.comPortNumber);
+                    break;
+                case ConnectionParams.IP ip:
+                    scu = new ScuSession(ip.IPAddress);
+                    break;
+            }
+        }
     }
 }
