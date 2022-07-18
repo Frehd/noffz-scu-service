42a84a4743d5f87cb978fc235979f563da5409be
Nikita Prorekhin <nikita.prorekhin@noffz.com>
2022-07-18 13:27:15 +0200

    Initial commit with solution and projects

---
+++ source/Noffz.SCU.Service/Noffz.SCU.Service.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noffz.SCU.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"NOFFZ SCU Service Console has been run. You passed the following arguments: {String.Join(",",args)}");
+            var service = new ScuService();
+        }
+    }
+}
